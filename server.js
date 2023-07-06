dc57679c95bf253a080da9ae46cba3b86d83851a
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-07-05 21:42:07 -0600

    Add route to test

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('This is working');
+});
+
 app.listen(3003, () => {
   console.log('Server is running on port 3003');
 });
