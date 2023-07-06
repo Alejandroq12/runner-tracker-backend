ed1795603c2936db0253eb570a7f3c765a734a85
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-07-05 21:49:05 -0600

    Plan API development

--- server.js
+++ server.js
@@ -9,3 +9,12 @@ app.get('/', (req, res) => {
 app.listen(3003, () => {
   console.log('Server is running on port 3003');
 });
+
+/*
+Routes:
+/ --> res = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> user
+*/
