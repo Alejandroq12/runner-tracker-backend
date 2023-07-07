49b18d14c5a8ad3060583df10022cc1fb2421004
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-07-07 15:45:48 -0600

    Erase comments

--- server.js
+++ server.js
@@ -88,12 +88,3 @@ app.put('/image', (req, res) => {
 app.listen(3003, () => {
   console.log('Server is running on port 3003');
 });
-
-/*
-Routes:
-/ --> res = this is working
-/signin --> POST = success/fail
-/register --> POST = user
-/profile/:userId --> GET = user
-/image --> PUT --> user
-*/
