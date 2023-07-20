9696147f05f17751ac4c90ff5a79a2e65a135978
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-07-19 18:51:18 -0600

    Add SQL command to alter table

--- schema.sql
+++ schema.sql
@@ -6,3 +6,5 @@ CREATE TABLE users(
 
 INSERT INTO users(name, age, birthday) VALUES ('Julio', 25, '1995-01-01');
 INSERT INTO users(name, age, birthday) VALUES ('Jennifer', 20, '1990-01-01');
+
+ALTER TABLE users ADD score smallint;
