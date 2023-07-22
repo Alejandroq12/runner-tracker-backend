961e63a88ce8ca40e5bb97e5c154a56cc87fe087
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-07-22 14:42:44 -0600

    Add table users

--- schema.sql
+++ schema.sql
@@ -36,3 +36,11 @@ DROP TABLE login;
 ---------------------------------------
 
 CREATE DATABASE "smart-recognizer";
+
+CREATE TABLE users (
+  id serial PRIMARY KEY,
+  name VARCHAR(100),
+  email text UNIQUE NOT NULL,
+  entries BIGINT DEFAULT 0,
+  joined TIMESTAMP NOT NULL
+);
