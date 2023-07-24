600a24bcdc8bf0cc13b448d15ccfca2e54543fa5
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-07-23 21:14:01 -0600

    Erase unnecesary code

--- server.js
+++ server.js
@@ -115,17 +115,6 @@ app.get('/profile/:id', (req, res) => {
 
 app.put('/image', (req, res) => {
   const { id } = req.body;
-  let found = false;
-  database.users.forEach((user) => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
-  });
-  if (!found) {
-    res.status(400).json('not found');
-  }
 });
 
 app.listen(3003, () => {
