e305c6d0399878be790e09dd5dc0612f35edd170
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-07-23 21:20:08 -0600

    Implement our where method to the database

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ app.get('/profile/:id', (req, res) => {
 
 app.put('/image', (req, res) => {
   const { id } = req.body;
-  .where('published_date', '<', 2000)
+  db.where('published_date', '<', 2000)
   .update({
     status: 'archived',
     thisKeyIsSkipped: undefined
