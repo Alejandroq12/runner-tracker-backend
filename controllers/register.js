6895bd1a01be213dcba0150922acca970672c0ce
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-03-08 23:52:09 -0600

    Feat: Add validation to the signup endpoint.

--- controllers/register.js
+++ controllers/register.js
@@ -1,5 +1,8 @@
 const handleRegister = (req, res, db, bcrypt, jwt) => {
   const { email, name, password } = req.body;
+  if (!email || !name || !password) {
+    return res.status(400).json('Incorrect form submision')
+  }
   const hash = bcrypt.hashSync(password);
   db.transaction(trx => {
       trx.insert({
