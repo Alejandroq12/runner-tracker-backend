a030be81c629b50c9e4dd7357223b47cc5aa194d
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-03-09 17:49:14 -0600

    Fix: Adding database connection.

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const validateJwt = require('./controllers/validateJwt');
 const db = knex({
   client: 'pg',
   connection: {
-    connectionString: 'process.env.DATABASE_URL',
+    connectionString: process.env.DATABASE_URL,
     ssl: true,
   },
 });
